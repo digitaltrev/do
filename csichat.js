03e9877e1d07a941fd060a4f7b8354aeb1d47216
digitaltrev <99693093+digitaltrev@users.noreply.github.com>
2022-05-11 12:46:17 -0700

    Add files via upload

---
+++ csichat.js
@@ -0,0 +1,40 @@
+
+rwc_connect_init={"init":{"captcha":1,"fileXfer":{"maxSendMB":-1,"allow":false,"extensions":null,"rights":"sendrecv"},"name":"","web_location":"https://chat.csinufund.com","emoticons":[["https:\/\/chat.csinufund.com\/emoticons\/smile.png","Smile face",":-)",0],["https:\/\/chat.csinufund.com\/emoticons\/bigsmile.png","Big smile face",":-D",0],["https:\/\/chat.csinufund.com\/emoticons\/surprised.png","Surprised face",":-O",0],["https:\/\/chat.csinufund.com\/emoticons\/wink.png","Wink face",";-)",0],["https:\/\/chat.csinufund.com\/emoticons\/sad.png","Sad face",":-(",1],["https:\/\/chat.csinufund.com\/emoticons\/confused.png","Confused face",":-S",0],["https:\/\/chat.csinufund.com\/emoticons\/disappointed.png","Disappointed face",":-|",0],["https:\/\/chat.csinufund.com\/emoticons\/embarrassed.png","Embarrassed face",":-$",0],["https:\/\/chat.csinufund.com\/emoticons\/cool.png","Cool smile face","(H)",0],["https:\/\/chat.csinufund.com\/emoticons\/zipmouth.png","Zipper mouth face",":-#",1],["https:\/\/chat.csinufund.com\/emoticons\/sarcastic.png","Sarcastic face","^o)",0],["https:\/\/chat.csinufund.com\/emoticons\/thinking.png","Thinking face","*-)",0],["https:\/\/chat.csinufund.com\/emoticons\/sleepy.png","Sleepy face","|-)",0],["https:\/\/chat.csinufund.com\/emoticons\/clock.png","Clock","(O)",0],["https:\/\/chat.csinufund.com\/emoticons\/coffee.png","Coffee","(C)",1],["https:\/\/chat.csinufund.com\/emoticons\/idea.png","Idea","(I)",0],["https:\/\/chat.csinufund.com\/emoticons\/thumbsup.png","Yes","(Y)",0],["https:\/\/chat.csinufund.com\/emoticons\/thumbsdown.png","No","(N)",0],["https:\/\/chat.csinufund.com\/emoticons\/question.png","Question","(?)",0],["https:\/\/chat.csinufund.com\/emoticons\/caution.png","Caution","{!}",1],["https:\/\/chat.csinufund.com\/emoticons\/star.png","Star","(*)",0],["https:\/\/chat.csinufund.com\/emoticons\/ftp.png","File Transfer","{ftp}",0],["https:\/\/chat.csinufund.com\/emoticons\/telephone.png","Telephone","(T)",0],["https:\/\/chat.csinufund.com\/emoticons\/share.png","Share Desktop","{share}",0],["https:\/\/chat.csinufund.com\/emoticons\/video.png","Video","{video}",0]],"clientTools":null,"voice":1,"video_send":1,"file_transfer_send":1,"remote_desktop_send":1,"results":"complete"}};
+
+var widgetParms = {
+    revBrand:"0471",
+    revAgentConnect:null,
+    revPopOut:"false",
+    revAuthMode:"anonymous",
+    revAuthToken:null,
+    revGroup:"0471",
+    revDstPid:"nufund_ai_bot@centier.com",
+    revVoice:null,
+    revVideo:null,
+    revVideoMix:null,
+    revRDS:null,
+    revConference:null,
+    revFileUpload:null,
+    revInitialHold:null,
+    revInitialMsg:null,
+    revVoiceAutostart:null,
+    revVideoAutostart:null,
+    revChatPanelOpen:null,
+    revEndSessionOnClose:null,
+    revInitialForm:{"group":"0471","locale":"en-us","rev-auth-mode":"anonymous","rev-brand":"0471","rev-dst-pid":"nufund_ai_bot@centier.com","rev-group":"0471","rev-pop-out":"false"},
+    revKeyPad:null
+};
+widgetBrand={chatTitle:'Chat',initialTitle:'Chat with an agent.',initialMsg:'Agents are standing by to answer your questions.'}
+if (typeof widgetBrand!=='object'||!widgetBrand){
+    widgetBrand={};
+}
+if (typeof widgetBrand.popLogo !== 'string') {
+    widgetBrand.popLogo='../0471/logo.png';
+}
+if (typeof widgetBrand.popLogoAlt !== 'string') {
+    widgetBrand.popLogoAlt='Centier Bank';
+}
+if (typeof widgetBrand.anchorsEnabledOnMobile !== 'string') {
+    widgetBrand.anchorsEnabledOnMobile='true';
+}
+widgetBrand.configCSS='../0471/config.css';
