8beee3c3f4bd266d02796d815e8e3e30b990fe82
digitaltrev <99693093+digitaltrev@users.noreply.github.com>
2023-10-12 12:05:14 -0700

    Update direct.js

--- direct.js
+++ direct.js
@@ -1,5 +1,7 @@
 window.onload = function(){
+    print("lets go")
     let button = document.querySelector('.MuiButton-sizeLarge');
+    print(button)
     if (button) {
         button.click();
     } else {
